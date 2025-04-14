9a7e8cef3273401af0509cf5be6c54097b0de168
Kiril Burinskij <151399511+pyro90@users.noreply.github.com>
2025-04-14 21:57:46 +0100

    Added a login / register + main page. Added a profile box which shows when clicking the profile area. General bug fixess implemented.

--- js/profile.js
+++ js/profile.js
@@ -1,13 +1,13 @@
 
   function toggleBox() 
   {
-    const box = document.getElementById('profileBox');
+    const box = document.getElementById('profile-box');
     box.style.display = box.style.display === 'block' ? 'none' : 'block';
   }
 
   document.addEventListener('click', function(e) {
     const profile = document.querySelector('.profile');
-    const box = document.getElementById('profileBox');
+    const box = document.getElementById('profile-box');
     if (!profile.contains(e.target) && !box.contains(e.target)) {
       box.style.display = 'none';
     }
